b16e3e610c14f81483b78675f3481c6363fe6c7b
Davis Haupt <dbh937@gmail.com>
2017-01-18 14:40:14 -0500

    taking an L

--- archonmovement/Archon.java
+++ archonmovement/Archon.java
@@ -4,11 +4,11 @@ import battlecode.common.*;
 
 public class Archon extends BaseRobot {
 	int gardenersMade = 0;
-    int broadcastNum = 0;
-    MapLocation[] gardeners = new MapLocation[20];
+	int broadcastNum = 0;
+	MapLocation[] gardeners = new MapLocation[20];
 	static float arcDirection = 4.0f;
 	Direction teamDir;
-    float lastHealth = 40000;
+	float lastHealth = 40000;
 	public Archon(RobotController rc) {
 		super(rc);
 	}
@@ -20,23 +20,23 @@ public class Archon extends BaseRobot {
 		}
 	}
 	void run() throws GameActionException {
-		for(int i = 100; i < GameConstants.BROADCAST_MAX_CHANNELS; i+=3){
+		for(int i = 100; i < 100+20*3; i+=3){
 			if(rc.readBroadcast(i) !=0){
 				if(i!= 100){
-					gardeners[(i-100)/4] =  new MapLocation(rc.readBroadcast(i), rc.readBroadcast(i+1));
+					gardeners[(i-100)/3] =  new MapLocation(rc.readBroadcast(i), rc.readBroadcast(i+1));
 				}
 			}
 		}
-        try {
-        	
-            float hp = rc.getHealth();
-            if(lastHealth < hp) {
-                rc.broadcast(15, 1);
-            }
-            lastHealth = hp;
-        } catch (GameActionException e) {
-            
-        }
+		try {
+
+			float hp = rc.getHealth();
+			if(lastHealth < hp) {
+				rc.broadcast(15, 1);
+			}
+			lastHealth = hp;
+		} catch (GameActionException e) {
+
+		}
 		if (gardenersMade < 8) {
 			arcMove();
 			tryGardener();	
@@ -75,7 +75,7 @@ public class Archon extends BaseRobot {
 						rc.hireGardener(dirList[0]);
 						gardenersMade++;
 						Clock.yield();
-					//}
+						//}
 					}
 				}
 			}
@@ -85,56 +85,61 @@ public class Archon extends BaseRobot {
 		}
 	}
 	void arcMove() {
-			try {	
-				 BulletInfo[] bullets = rc.senseNearbyBullets();
-		            if(bullets.length>0){
-		            	Direction newDir = rc.getLocation().directionTo(bullets[0].location).rotateLeftDegrees(90);
-		            	Direction newDir2 = rc.getLocation().directionTo(bullets[0].location).rotateLeftDegrees(90);
-		            	Direction newDir3 = rc.getLocation().directionTo(bullets[0].location).rotateRightDegrees(90);
-		            	if(rc.canMove(newDir)){
-		            		rc.move(newDir);
-		            		Clock.yield();
-		            	}
-		            	else if(rc.canMove(newDir2)){
-		            		rc.move(newDir2);
-		            		Clock.yield();
-		            	}
-		            	else if(rc.canMove(newDir3)){
-		            		rc.move(newDir3);
-		            		Clock.yield();
-		            	}
-		            }
-				Direction dir;
-				float avg_x = 0;
-				int num_gardeners = 0;
-				float avg_y = 0;
-
-				for(int i = 0; i < gardeners.length ; i++){
-					if(gardeners[i]!= null){
-						avg_x += gardeners[i].x;
-						avg_y += gardeners[i].y;
-						num_gardeners++;
-					}
+		try {	
+			BulletInfo[] bullets = rc.senseNearbyBullets();
+			if(bullets.length>0){
+				Direction newDir = rc.getLocation().directionTo(bullets[0].location).rotateLeftDegrees(90);
+				Direction newDir2 = rc.getLocation().directionTo(bullets[0].location).rotateLeftDegrees(90);
+				Direction newDir3 = rc.getLocation().directionTo(bullets[0].location).rotateRightDegrees(90);
+				if(rc.canMove(newDir)){
+					rc.move(newDir);
+					Clock.yield();
 				}
+				else if(rc.canMove(newDir2)){
+					rc.move(newDir2);
+					Clock.yield();
+				}
+				else if(rc.canMove(newDir3)){
+					rc.move(newDir3);
+					Clock.yield();
+				}
+			}
+			Direction dir;
+			float avg_x = 0;
+			int num_gardeners = 0;
+			float avg_y = 0;
+
+			for(int i = 0; i < gardeners.length ; i++){
+				if(gardeners[i]!= null){
+					avg_x += gardeners[i].x;
+					avg_y += gardeners[i].y;
+					num_gardeners++;
+				}
+			}
+			if (num_gardeners > 0) {
 				avg_x = avg_x/num_gardeners;
 				avg_y = avg_y/num_gardeners;
 				MapLocation gardener_target = new MapLocation(avg_x, avg_y);
 				if(num_gardeners == 0){
-				dir = new Direction(arcDirection);
+					dir = new Direction(arcDirection);
 					if(rc.canMove(dir)) {
 						rc.move(dir);
 					} else {
 						arcDirection = (float) (arcDirection + Math.PI/((Math.random()* 2 + 4)));
 					}
 				}else{
-					if(rc.canMove(rc.getLocation().directionTo(gardener_target)) && !rc.hasMoved()){
+					try {
 						rc.move(gardener_target);
 						rc.setIndicatorDot(gardener_target, 1000, 0, 1000);
+					} catch(GameActionException e) {
+						e.printStackTrace();
 					}
+					
 				}
-			} catch (GameActionException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
 			}
+		} catch (GameActionException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 }
